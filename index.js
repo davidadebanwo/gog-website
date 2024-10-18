1da18de740188986ec0c4b26ce73ec9e2568dd56
David Adebanwo <davidadebanwo@gmail.com>
2024-10-18 04:24:20 +0100

    First commit. Worked on the homepage route and fontend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+const PORT = 2000;
+app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`));
